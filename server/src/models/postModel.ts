cac58867a25ebcd97c92e3eddb1401dcddd5e130
shemilkumar <ekaravindhakshan@gmail.com>
2023-05-12 00:47:49 +0530

    Connected Post and Comments virtualy

--- server/src/models/postModel.ts
+++ server/src/models/postModel.ts
@@ -1,34 +1,49 @@
 import mongoose, { Query } from "mongoose";
 import { PostModel } from "../types/modelTypes";
 
-const postSchema = new mongoose.Schema<PostModel>({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: [true, "A Post belongs to a user"],
+const postSchema = new mongoose.Schema<PostModel>(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "A Post belongs to a user"],
+    },
+    description: {
+      type: String,
+      required: [true, "A post must contain description"],
+    },
+    photo: {
+      type: String,
+      required: [false],
+    },
+    likes: {
+      type: [mongoose.Schema.Types.ObjectId],
+      default: [],
+    },
+    postedAt: {
+      type: Date,
+      default: Date.now(),
+    },
   },
-  description: {
-    type: String,
-    required: [true, "A post must contain description"],
-  },
-  photo: {
-    type: String,
-    required: [false],
-  },
-  likes: {
-    type: [mongoose.Schema.Types.ObjectId],
-    default: [],
-  },
-  postedAt: {
-    type: Date,
-    default: Date.now(),
-  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+postSchema.virtual("comments", {
+  ref: "Comment",
+  foreignField: "post",
+  localField: "_id",
 });
 
 postSchema.pre(/^find/, function (this: Query<PostModel[], PostModel>, next) {
   this.populate({
     path: "user",
     select: "name photo",
+  }).populate({
+    path: "comments",
+    select: "user comment commentedAt -post",
   });
   next();
 });
